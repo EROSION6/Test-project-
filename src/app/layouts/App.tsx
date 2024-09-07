3dc077a71d2c027eacf00474f826d4e1931f659a
EROSION6 <147639244+EROSION6@users.noreply.github.com>
2024-09-07 23:52:47 +0300

    New commit test-project

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,13 +1,3 @@
 export const App = () => {
-	return (
-		<div>
-			<div>
-				<h1>
-					Welcome to Bro Test! This is the App component.
-					<br />
-					This is a test for the bro build system.
-				</h1>
-			</div>
-		</div>
-	)
+	return <div>......</div>
 }
